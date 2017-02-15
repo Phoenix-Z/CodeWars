ca828f074b51d88e83727dbef2d4ef2caf9e77a6
Phoenix-Z <phoenixzhao1105@gmail.com>
2017-02-15 23:19:37 +0800

    clean javascript solution to adding big numbers.

---
+++ AddingBigNumbers/solution.js
@@ -0,0 +1,12 @@
+// 这是用JavaScript写大数加法一个非常简洁的一个做法
+function add (a, b) {
+  var res = '', c = 0
+  a = a.split('')
+  b = b.split('')
+  while (a.length || b.length || c) {
+    c += ~~a.pop() + ~~b.pop()
+    res = c % 10 + res
+    c = c > 9
+  }
+  return res
+}
